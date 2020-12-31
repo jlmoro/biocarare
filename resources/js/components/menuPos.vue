88951e1c1dcf2df72101319b74fbcaf2bbdbc4e4
Jose L. Moreno <jose0909@live.com>
2020-12-31 13:00:46 -0500

    ajuste en el menu, recorriendo array

--- resources/js/components/menuPos.vue
+++ resources/js/components/menuPos.vue
@@ -6,41 +6,11 @@
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
           <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
+              <li v-for="(data,m) in dataMenu" :key="m"  class="nav-item">
+                <router-link class="nav-link" :to="{ name: data.ruta}">            
                   <span data-feather="home"></span>
-                  Dashboard <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file"></span>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="shopping-cart"></span>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
+                  {{data.nombre_item}} <span class="sr-only"></span>
+                </router-link>
               </li>
             </ul>
 
@@ -93,9 +63,19 @@ export default {
     }
   },
 
-  computed: mapGetters({
-    user: 'auth/user'
-  }),
+  computed:{
+    ...mapGetters({
+      user: 'auth/user',
+      dataMenu: 'menu/getMenu'
+    }),
+  },
+
+  created() {
+    if(this.$store.getters['auth/check']){
+      this.$store.dispatch('menu/listarMenu')
+    }
+  },
+
 
 }
 </script>
