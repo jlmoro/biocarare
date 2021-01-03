55717dae05ed52f62be6ab3f850cc2bc7e4eacf8
Jose L. Moreno <jose0909@live.com>
2021-01-03 08:30:27 -0500

    instalación de amcharts

---
+++ resources/js/pages/dashboard/principal.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="dashboard-principal">
+    <h4>dashboard</h4>
+  </section>
+</template>
+<script>
+export default {
+  name: "",
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/router/routes/dashboard.js
@@ -0,0 +1,8 @@
+import page from '../page'
+export default [
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: page('dashboard/principal'),
+  },
+]
