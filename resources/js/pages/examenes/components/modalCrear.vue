a6f0d25e43166beb444806d5cc364d86832949d4
Jose L. Moreno <jose0909@live.com>
2021-01-12 14:23:26 -0500

    ajuste en modal registrar examen

--- resources/js/pages/examenes/components/modalCrear.vue
+++ resources/js/pages/examenes/components/modalCrear.vue
@@ -1,66 +1,52 @@
 <template>
   <section>
     <modal-form ref="modalCrear" size="md">
+      <div slot="header" class="">
+        <span class="letra-capital">registrar examen</span>
+      </div>
+      <div slot="body" class="">
 
-      <b-card bg-variant="light">
-    <b-form-group
-      label-cols-lg="3"
-      label="Shipping Address"
-      label-size="lg"
-      label-class="font-weight-bold pt-0"
-      class="mb-0"
-    >
-      <b-form-group
-        label="Street:"
-        label-for="nested-street"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="nested-street"></b-form-input>
-      </b-form-group>
 
-      <b-form-group
-        label="City:"
-        label-for="nested-city"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="nested-city"></b-form-input>
-      </b-form-group>
+          <!-- <b-form-group label-cols-lg="3" class="mb-0" > -->
+          <b-form-group class="mb-0" >
 
-      <b-form-group
-        label="State:"
-        label-for="nested-state"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="nested-state"></b-form-input>
-      </b-form-group>
+            <b-form-group label="Street:" label-for="nested-street" label-cols-sm="3" label-align-sm="right" >
+              <b-form-input id="nested-street"></b-form-input>
+            </b-form-group>
 
-      <b-form-group
-        label="Country:"
-        label-for="nested-country"
-        label-cols-sm="3"
-        label-align-sm="right"
-      >
-        <b-form-input id="nested-country"></b-form-input>
-      </b-form-group>
+            <b-form-group label="City:" label-for="nested-city" label-cols-sm="3" label-align-sm="right" >
+              <b-form-input id="nested-city"></b-form-input>
+            </b-form-group>
 
-      <b-form-group
-        label="Ship via:"
-        label-cols-sm="3"
-        label-align-sm="right"
-        class="mb-0"
-        v-slot="{ ariaDescribedby }"
-      >
-        <b-form-radio-group
-          class="pt-2"
-          :options="['Air', 'Courier', 'Mail']"
-          :aria-describedby="ariaDescribedby"
-        ></b-form-radio-group>
-      </b-form-group>
-    </b-form-group>
-  </b-card>
+            <b-form-group label="State:" label-for="nested-state" label-cols-sm="3" label-align-sm="right" >
+              <b-form-input id="nested-state"></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Country:" label-for="nested-country" label-cols-sm="3" label-align-sm="right" >
+              <b-form-input id="nested-country"></b-form-input>
+            </b-form-group>
+
+            <b-form-group label="Ship via:" label-cols-sm="3" label-align-sm="right" class="mb-0" v-slot="{ ariaDescribedby }" >
+              <b-form-radio-group
+              class="pt-2"
+              :options="['Air', 'Courier', 'Mail']"
+              :aria-describedby="ariaDescribedby">
+            </b-form-radio-group>
+          </b-form-group>
+          </b-form-group>
+
+
+      </div>
+      <div slot="footer" class="">
+        <button type="button" class="btn-guardar">
+          <fa icon="check"/>
+          <span class="letra-capital">registrar</span>
+        </button>
+        <button type="button" class="btn-cancelar" data-dismiss="modal" aria-label="Close">
+          <fa icon="times"/>
+          <span class="letra-capital">cancelar</span>
+        </button>
+      </div>
 
     </modal-form>
   </section>
@@ -77,7 +63,6 @@ export default {
     toggle() {
       this.$refs.modalCrear.toggle()
     }
-
   }
 }
 </script>
